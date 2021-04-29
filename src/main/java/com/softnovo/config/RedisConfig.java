003a81fb32c5349802ff9b92562cd39e9a725dd2
cgm <plextocao@gmail.com>
2021-04-29 19:09:59 +0800

    redis

--- src/main/java/com/softnovo/config/RedisConfig.java
+++ src/main/java/com/softnovo/config/RedisConfig.java
@@ -6,12 +6,11 @@ import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.lang.reflect.Method;
-
 @Configuration
 @EnableCaching
 public class RedisConfig extends CachingConfigurerSupport {
     @Bean
+    @Override
     public KeyGenerator keyGenerator() {
         return (target, method, params) -> {
             StringBuilder sb = new StringBuilder();
